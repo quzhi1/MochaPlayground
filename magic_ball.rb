e6b45d9f68fd3c207cf01c059c62ee8fdeedf920
Zhi Qu <zhiqu@stripe.com>
2019-05-30 14:52:42 -0700

    First commit

---
+++ magic_ball.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# MagicBall
+class MagicBall
+  ANSWERS = ['It is certain',
+             'It is decidedly so',
+             'Without a doubt',
+             'Yes, definitely',
+             'You may rely on it',
+             'As I see it, yes',
+             'Most likely',
+             'Outlook good',
+             'Yes',
+             'Signs point to yes',
+             'Reply hazy try again',
+             'Ask again later',
+             'Better not tell you now',
+             'Cannot predict now',
+             'Concentrate and ask again',
+             "Don't count on it",
+             'My reply is no',
+             'My sources say no',
+             'Outlook not so good',
+             'Very doubtful'].freeze
+
+  def ask(question)
+    raise 'Question has invalid format.' \
+    unless question.is_a?(String) && question[-1] == '?'
+
+    ANSWERS.sample
+  end
+end
